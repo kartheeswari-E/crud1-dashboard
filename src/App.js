45c34ac5c7946e3a666c0e3e3d64e48e779f89ce
kartheeswari <kartheesw2001@gmail.com>
2022-11-14 01:15:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import Dashboard from './component/Dashboard';
+import Sidebar from './component/Sidebar';
 import './App.css';
+import Edit from './component/Edit';
+import Adduser from './component/Adduser';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  var data={
+    montly:"45,000",
+    annual:"12,00,000",
+    task:20,
+    pending:18
+  };
+  let [user,setuser]=useState([{
+    name:"karthiga",
+    email:"kar@gmail.com",
+    mobile:"9344245002"
+  },
+{
+  name:"karthiga rani",
+  email:"karrani@gmail.com",
+ mobile:"9678901987"
+}])
+  return <>
+  <div id='wrapper'>
+    <BrowserRouter>
+<Sidebar/>
+<Routes>
+<Route path='/dashboard' element={<Dashboard data={{data,user,setuser}} />} />
+<Route path='/add-user' element={<Adduser user={{user,setuser}}/>} />
+<Route path='/edit/:id' element={<Edit user={{user,setuser}}/>} />
+</Routes>
+</BrowserRouter>
+</div>
+
+  </>
 }
 
 export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+function Home() {
+
+  return <>
+  <Link to='/home'>
+  <div>
+  <h1>Welcome To Dashboard CRUD Application </h1>
+  </div></Link>
+  </>
+}
+
+export default Home
